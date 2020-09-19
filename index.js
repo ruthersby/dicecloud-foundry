a0eb024a0bbbb5f9952a5a2b719b5ca6e716f0d8
Tim Ruthersby <truthe01@harris.com>
2020-09-19 16:40:41 -0600

    button added to character sheet header

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("ruthersby load");
+
+Hooks.on("init", function() {
+  console.log("ruthersby init");
+});
+
+Hooks.on("ready", function() {
+  console.log("ruthersby ready");
+  let pcCharacters = Object.values(CONFIG.Actor.sheetClasses.character).map(s=>s.cls.name);
+  pcCharacters.forEach(sheet => {
+      Hooks.on('render'+sheet, (app, html, data) => {
+          let button = $('<button type="button" id="dicecloud-config" class="dicecloud"/>');
+          $(html).find('.window-title').after(button);
+
+          button.click(()=> {
+              console.log("Button Clicked");
+          })
+      });
+  });
+});
